4c5e2c3d820737fa072beafaa90c8d419c369792
pradeep113 <pradeep.prdp113@gmail.com>
2020-08-25 17:40:32 +0530

    Home and About Html files

--- test_project/pages/views.py
+++ test_project/pages/views.py
@@ -1,6 +1,13 @@
  # pages/views.py
 
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
-def homePageView(request):
-	return HttpResponse('Hello, World!')
+#def homePageView(request):
+#	return HttpResponse('Hello, World!')
+
+from django.views.generic import TemplateView
+class homePageView(TemplateView): 
+	template_name = 'home.html'
+
+class AboutPageView(TemplateView): 
+	template_name = 'about.html'
